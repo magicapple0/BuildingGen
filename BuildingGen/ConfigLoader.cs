7f263dc08654570eae49b9f14bd568fbc1aeca29
magicapple0 <46961647+magicapple0@users.noreply.github.com>
2024-04-15 19:44:10 +0500

    Добавьте файлы проекта.

---
+++ BuildingGen/ConfigLoader.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace BuildingGen
+{
+    public class ConfigLoader 
+    {
+        public InputJson InputJson { get; private set; }
+        public List<Tile> Tiles { get; private set; }
+
+        private readonly string fileName;
+        public ConfigLoader(string fileName) {
+            this.fileName = fileName;
+            InputJson = JsonSerializer.Deserialize<InputJson>(File.ReadAllText(fileName));
+            Tiles = new List<Tile>();
+            //var currTile = TileInfoToTile(InputJson.Tiles.FirstOrDefault());
+            //var modifiers = new[] { None, FlipXTile, FlipYTile, FlipZTile, RotateXTile };
+            //var modifiers = new[] {None, FlipXTile, FlipYTile, FlipZTile, RotateXTile, RotateXTile, RotateXTile, RotateYTile, RotateYTile, RotateYTile, RotateZTile, RotateZTile, RotateZTile };
+            //var automat = new Automatoe(modifiers);
+            foreach (var tile in InputJson.Tiles)
+            {
+                var tiles = new List<Tile>
+                {
+                    TileInfoToTile(tile)
+                };
+                if (tile.FlipX)
+                {
+                    tiles.Add(FlipXTile(tiles[0]));
+                }
+                if (tile.RotateZ)
+                {
+                    tiles.Add(RotateXTile(tiles[0]));
+                    tiles.Add(RotateXTile(tiles[tiles.Count - 1]));
+                    tiles.Add(RotateXTile(tiles[tiles.Count - 1]));
+                }
+                if (tile.RotateZ && tile.FlipX)
+                {
+                    tiles.Add(RotateXTile(tiles[1]));
+                    tiles.Add(RotateXTile(tiles[tiles.Count - 1]));
+                    tiles.Add(RotateXTile(tiles[tiles.Count - 1]));
+                }
+                Tiles.AddRange(tiles);
+            }
+        }
+
+
+        public string GetJsonString()
+        {
+            return File.ReadAllText(fileName);
+        }
+
+        private Tile TileInfoToTile(TileInfo tileInfo)
+        {
+            return new Tile(tileInfo, tileInfo.Edges.ToArray(), new List<TileModifiers>());
+        }
+
+        private Tile None(Tile tile)
+        {
+            var newEdges = new string[][] { tile.ModifiedEdges[0], tile.ModifiedEdges[1], tile.ModifiedEdges[2], tile.ModifiedEdges[3], tile.ModifiedEdges[4], tile.ModifiedEdges[5] };
+            var newModifiers = tile.TileModifiers.ToList();
+            newModifiers.Add(TileModifiers.None);
+            return new Tile(tile.TileInfo, newEdges, newModifiers);
+        }
+
+        private Tile FlipXTile(Tile tile)
+        {
+            var newEdges = new string[][] { tile.ModifiedEdges[0], tile.ModifiedEdges[1], tile.ModifiedEdges[2], tile.ModifiedEdges[4], tile.ModifiedEdges[3], tile.ModifiedEdges[5] };
+            var newModifiers = tile.TileModifiers.ToList();
+            newModifiers.Add(TileModifiers.FlipX);
+            return new Tile(tile.TileInfo, newEdges, newModifiers);
+        }
+
+
+        private Tile FlipYTile(Tile tile)
+        {
+            var newEdges = new string[][] { tile.ModifiedEdges[0], tile.ModifiedEdges[5], tile.ModifiedEdges[2], tile.ModifiedEdges[3], tile.ModifiedEdges[4], tile.ModifiedEdges[1] };
+            var newModifiers = tile.TileModifiers.ToList();
+            newModifiers.Add(TileModifiers.FlipY);
+            return new Tile(tile.TileInfo, newEdges, newModifiers);
+        }
+
+        private Tile FlipZTile(Tile tile)
+        {
+            var newEdges = new string[][] { tile.ModifiedEdges[2], tile.ModifiedEdges[1], tile.ModifiedEdges[0], tile.ModifiedEdges[3], tile.ModifiedEdges[4], tile.ModifiedEdges[5] };
+            var newModifiers = tile.TileModifiers.ToList();
+            newModifiers.Add(TileModifiers.FlipZ);
+            return new Tile(tile.TileInfo, newEdges, newModifiers);
+        }
+
+        private Tile RotateXTile(Tile tile)
+        {
+            var newEdges = new string[][] { tile.ModifiedEdges[4], tile.ModifiedEdges[1], tile.ModifiedEdges[3], tile.ModifiedEdges[0], tile.ModifiedEdges[2], tile.ModifiedEdges[5] };
+            var newModifiers = tile.TileModifiers.ToList();
+            newModifiers.Add(TileModifiers.RotateX);
+            return new Tile(tile.TileInfo, newEdges, newModifiers);
+        }
+
+        private Tile RotateYTile(Tile tile)
+        {
+            var newEdges = new string[][] { tile.ModifiedEdges[5], tile.ModifiedEdges[0], tile.ModifiedEdges[1], tile.ModifiedEdges[3], tile.ModifiedEdges[4], tile.ModifiedEdges[2] };
+            var newModifiers = tile.TileModifiers.ToList();
+            newModifiers.Add(TileModifiers.RotateY);
+            return new Tile(tile.TileInfo, newEdges, newModifiers);
+        }
+
+        private Tile RotateZTile(Tile tile)
+        {
+            var newEdges = new string[][] { tile.ModifiedEdges[0], tile.ModifiedEdges[3], tile.ModifiedEdges[2], tile.ModifiedEdges[5], tile.ModifiedEdges[1], tile.ModifiedEdges[4] };
+            var newModifiers = tile.TileModifiers.ToList();
+            newModifiers.Add(TileModifiers.RotateZ);
+            return new Tile(tile.TileInfo, newEdges, newModifiers);
+        }
+    }
+}
---
+++ BuildingGen/TileInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingGen
+{
+    public class TileInfo
+    {
+        public string Name { get; set; }
+        public string[][] Edges { get; set; }
+        public bool FlipX { get; set; }
+        public bool RotateZ { get; set; }
+
+        public TileInfo() { }
+        public TileInfo(string name, string[][] edges, bool flipX, bool rotateZ)
+        {
+            Name = name;
+            Edges = edges;
+            FlipX = flipX;
+            RotateZ = rotateZ;
+        }
+    }
+}
